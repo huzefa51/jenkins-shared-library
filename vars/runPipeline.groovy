4fc21286b6043ad4df528d599e881c73fdf7d42b
huzefa51 <huzefa.hamdard53@gmail.com>
2020-01-05 17:37:41 +0530

    Update runPipeline.groovy

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -2,28 +2,4 @@
 
 def call() {
     echo "hello bro"
-    stages {
-        stage ('Compile Stage') {
-            steps {
-                withMaven(maven : 'maven') {
-                    sh 'mvn clean compile'
-                }
-            }
-        }
-        stage ('Testing Stage') {
-
-            steps {
-                withMaven(maven : 'maven') {
-                    sh 'mvn test'
-                }
-            }
-        }
-        stage ('Install Stage') {
-            steps {
-                withMaven(maven : 'maven') {
-                    sh 'mvn install'
-                }
-            }
-        }
-    }
 }
