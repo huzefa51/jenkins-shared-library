0333359dde3e7c653ca9ce0b55a8eea3f636688d
huzefa51 <huzefa.hamdard53@gmail.com>
2020-01-05 16:57:20 +0530

    Update runPipeline.groovy

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,9 +1,6 @@
 def call() {
-pipeline {
-    agent any
     stages {
         stage ('Compile Stage') {
-
             steps {
                 withMaven(maven : 'maven') {
                     sh 'mvn clean compile'
